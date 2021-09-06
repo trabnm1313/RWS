e56288044d97aa249c14ddc683b5a2f7daf29a12
Bot-Bank100 <59901053+Bank1250@users.noreply.github.com>
2021-09-07 00:23:40 +0700

    Units Design

--- assets/entities/test.js
+++
@@ -1,7 +0,0 @@
-import { Image } from 'react-native'
-
-export default function (){
-    return {
-        renderer: <Image source={require('../image/Knight.png')} style={{ left: 50, top: 50, position: 'absolute', width: 100, height: 100}}></Image>
-    }
-}
