095a5d2727800c8b7a526824b9eb789103778859
Ketsaraporn Dhetboon <56309662+IVizzReview@users.noreply.github.com>
2021-09-07 23:15:03 +0700

    Add Monster

--- Constants.js
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from "react-native";
-
-export default {
-    MAX_WIDTH: Dimensions.get("screen").width,
-    MAX_HEIGHT: Dimensions.get("screen").height
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# RTS
--- This is a game develop by students using React Native and React Game Engine --
