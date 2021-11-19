f61c196f1eb2641f969b4292ca3e6e62f51c20ee
trabnm1313 <jame.sou.com@hotmail.com>
2021-11-19 22:36:13 +0700

    add array for item

--- Constants.js
+++ Constants.js
@@ -4,11 +4,13 @@ import React from "react"
 let stage = "Menu"
 let Level = 1
 let team = []
+let item = []
 
 export default {
     MAX_WIDTH: Dimensions.get("screen").width - 100,
     MAX_HEIGHT: Dimensions.get("screen").height,
     stage,
     Level,
-    team
+    team,
+    item
 }
