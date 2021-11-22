0adc234b22814762b005f42ba2e3227424707605
it62070122 <fookkub1@gmail.com>
2021-11-22 19:44:51 +0700

    :white_check_mark: change database
     change from using SQLite to the data that SQLite query in loading screen

--- assets/systems/genWord.js
+++ assets/systems/genWord.js
@@ -1,9 +1,9 @@
 import * as SQLite from "expo-sqlite";
+import { Data } from "./opendatabase";
 
 //Sample of how to get data from transaction to use
 
 export default async () => {
-
   // Random number in range (min, max) -> getRndInteger(0, 10) = random number between 0 -> 10
   function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -14,71 +14,52 @@ export default async () => {
     return String.fromCharCode(getRndInteger(65, 90));
   }
 
-  // Get word via first character
-  async function getWordSpecifyChar(char) {
-    const db = SQLite.openDatabase("dictionary.db");
-
-    return new Promise((resolve) => {
-      //Making transaction
-      db.transaction((tx) => {
-        tx.executeSql(
-          "SELECT word FROM entries where word like ?",
-          [char],
-          (_, res) => {
-            //Send it to resolve
-            resolve(res);
-          },
-          (_, err) => console.log(err)
-        );
-      });
-    });
-  }
-
   let Word = [];
   let result = [];
   let total = 0;
 
-  // Get first word
-  let gotWord = await getWordSpecifyChar(makeChar() + "%");
-
-  // Random extract Word from SQL Array
-  let theWord = gotWord.rows._array[getRndInteger(0, gotWord.rows._array.length)].word;
+  // Get first word from random makeChar
+  let query = Data.rows._array.filter(
+    (element) => element.word[0] == makeChar()
+  );
+  // Random Word from previous query
+  let theWord = query[getRndInteger(0, query.length)].word;
 
   // Total length tell that how many space left
   total += theWord.length;
 
   // Push that word into array of word
-  Word.push(theWord)
+  Word.push(theWord);
 
-  // Get other word and push it in array until array have 15 letter
+  // Get other word and push it in array until array have 18 letter
   // The array gonna looks like this => array = [ "first_word (from lines 61 -> 71)", "second (in-process below)", "third (in-process below)"]
   while (total < 18) {
-    gotWord = await getWordSpecifyChar(makeChar() + "%")
+    query = Data.rows._array.filter((element) => element.word[0] == makeChar());
 
-    if (gotWord.rows._array.length === 0) {
-     continue
+    if (query.length === 0) {
+      continue;
     } else {
-      theWord = gotWord.rows._array[getRndInteger(0, gotWord.rows._array.length)].word;
+      theWord = query[getRndInteger(0, query.length)].word;
       total += theWord.length;
-      Word.push(theWord)
+      Word.push(theWord);
     }
   }
 
   // split the word to letter array => ["L", "O", "V", "E"]
   Word.map((each) => {
-    let spilter = each.split('');
+    let spilter = each.split("");
     spilter.map((letter) => {
-      result.push(letter.toUpperCase())
-    })
-  })
+      result.push(letter.toUpperCase());
+    });
+  });
 
   // delete the overflow letter
   if (result.length > 18) {
-    result.splice(18, result.length - 18)
+    result.splice(18, result.length - 18);
   }
 
   // shuffle element in array
-  final_result = result.sort(() => Math.random() - 0.5)
+  let final_result = result.sort(() => Math.random() - 0.5);
 
-  return final_result
+  return final_result;
 };
